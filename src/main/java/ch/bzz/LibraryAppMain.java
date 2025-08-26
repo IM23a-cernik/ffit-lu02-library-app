d6ad6f3c3756b92001b4fbf621c5f3342eac8b6a
KrisHD1337 <kriscerni@gmail.com>
2025-08-26 09:24:40 +0200

    Implemented Scanner

--- src/main/java/ch/bzz/LibraryAppMain.java
+++ src/main/java/ch/bzz/LibraryAppMain.java
@@ -1,8 +1,26 @@
 package ch.bzz;
 
+
+import java.util.Scanner;
+
 public class LibraryAppMain {
 
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        String selection = "";
+        String[] commands = {"help", "quit"};
+        Scanner console = new Scanner(System.in);
+        while (!selection.equals("quit")) {
+            System.out.println("Gib einen Befehl ein: ");
+            selection = console.nextLine();
+            if (selection.equals("quit")) {
+                System.out.println("Programm beendet.");
+            } else if (selection.equals("help")) {
+                for (String command : commands) {
+                    System.out.println(command);
+                }
+            } else {
+                System.out.println(selection+" wurde nicht erkannt.");
+            }
+        }
     }
 }
